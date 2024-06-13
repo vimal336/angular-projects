ba5a2f568e4304d1dd91ae33933b71768f058a65
vimal336 <asvimal336@gmail.com>
2024-06-13 12:38:40 +0530

    new component

---
+++ responsive-site/src/app/angular-directives/angular-directives.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AngularDirectivesComponent } from './angular-directives.component';
+
+describe('AngularDirectivesComponent', () => {
+  let component: AngularDirectivesComponent;
+  let fixture: ComponentFixture<AngularDirectivesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AngularDirectivesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AngularDirectivesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ responsive-site/src/app/angular-directives/angular-directives.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-directives',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-directives.component.html',
+  styleUrl: './angular-directives.component.css'
+})
+export class AngularDirectivesComponent {
+ Title = "Directives"
+}
