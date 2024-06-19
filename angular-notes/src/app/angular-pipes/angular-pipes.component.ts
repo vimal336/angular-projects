579f533f26975357d48b353b220196f82cbc3a3b
vimal336 <asvimal336@gmail.com>
2024-06-19 11:58:30 +0530

    pipes in angular

--- angular-notes/src/app/angular-pipes/angular-pipes.component.ts
+++ angular-notes/src/app/angular-pipes/angular-pipes.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './angular-pipes.component.html',
   styleUrl: './angular-pipes.component.css'
 })
 export class AngularPipesComponent {
-
+ ucasepipe:string = "hello world";
 }
