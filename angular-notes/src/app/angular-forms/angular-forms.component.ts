98831b1a4275270dedbc3377334eeec449fb4266
vimal336 <asvimal336@gmail.com>
2024-06-21 11:44:14 +0530

    forms in angular

--- angular-notes/src/app/angular-forms/angular-forms.component.ts
+++ angular-notes/src/app/angular-forms/angular-forms.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-angular-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './angular-forms.component.html',
   styleUrl: './angular-forms.component.css'
 })
 export class AngularFormsComponent {
+
+  constructor(){}
+
+   
+  submit(form:NgForm){
+
+  }
 
 }
