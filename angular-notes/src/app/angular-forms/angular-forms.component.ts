ab8da164c9516eb178f90b5f5cfabe50d4551af3
vimal336 <asvimal336@gmail.com>
2024-06-23 18:01:29 +0530

    observable

--- angular-notes/src/app/angular-forms/angular-forms.component.ts
+++ angular-notes/src/app/angular-forms/angular-forms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-angular-forms',
@@ -14,7 +15,10 @@ export class AngularFormsComponent implements OnInit {
   constructor(){}
 
   ngOnInit(): void {
-    
+    const myobs$ = new Observable(obs=>{
+      console.log("my observable");
+      obs.next('100')
+    })
   }
 
    
