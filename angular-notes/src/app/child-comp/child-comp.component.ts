158e78a69371e9a0e1e53b9da7eaa3a4fc2e66f6
vimal336 <asvimal336@gmail.com>
2024-06-24 09:47:32 +0530

    forms in angular

---
+++ angular-notes/src/app/child-comp/child-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './child-comp.component.html',
+  styleUrl: './child-comp.component.css'
+})
+export class ChildCompComponent {
+
+}
---
+++ angular-notes/src/app/parent-comp/parent-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent-comp.component.html',
+  styleUrl: './parent-comp.component.css'
+})
+export class ParentCompComponent {
+
+}
