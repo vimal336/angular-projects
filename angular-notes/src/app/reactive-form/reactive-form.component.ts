2ae497000473952be174d69a1b46f463a8f76ef6
vimal336 <asvimal336@gmail.com>
2024-06-27 11:23:28 +0530

    observable

--- angular-notes/src/app/reactive-form/reactive-form.component.ts
+++ angular-notes/src/app/reactive-form/reactive-form.component.ts
@@ -46,6 +46,56 @@ export class ReactiveFormComponent {
     
   });
 
+  contactForm = new FormGroup({
+    firstName : new FormControl('',[Validators.required, Validators.minLength(2)]),
+    lastName: new FormControl({
+        value: 'Vimal',
+        disabled: false
+    },[Validators.required, Validators.maxLength(15), Validators.pattern("^[a-zA-Z]+$")]),
+    email: new FormControl("",[Validators.required]),
+    gender: new FormControl("",[Validators.required]),
+    isMarried: new FormControl("",[Validators.requiredTrue]),
+    country: new FormControl("",[Validators.required]),
+    address: new FormGroup({
+      city: new FormControl("",[Validators.required]),
+      street: new FormControl("",[Validators.required]),
+      pincode:new FormControl("",[Validators.required])
+    })
+})
+
+get firstname() {
+  return this.contactForm.get('firstName')
+}
+get lastname() {
+  return this.contactForm.get('lastName')
+}
+get email() {
+  return this.contactForm.get('email')
+}
+get gender() {
+  return this.contactForm.get('gender')
+}
+get marriedStatus() {
+  return this.contactForm.get('isMarried')
+}
+get country() {
+  return this.contactForm.get('country')
+}
+get city() {
+  return this.contactForm.get('address')?.get('city')
+}
+get street() {
+  return this.contactForm.get('address')?.get('street')
+}
+get pincode() {
+  return this.contactForm.get('address')?.get('pincode')
+}
+
+onSubmit() {
+    console.log(this.contactForm.value)
+}
+
+
   // onSubmit() {
   //   if (this.createForm.valid) {
   //     console.log(this.createForm.value);
@@ -54,8 +104,6 @@ export class ReactiveFormComponent {
   //   }
   // }
 
-  onSubmit() {
-    console.log(this.profileForm.value);
-  }
+  
 
 }
