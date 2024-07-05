5438b41a94dc8ead018a039ea389414daf27a178
vimal336 <asvimal336@gmail.com>
2024-07-05 12:13:26 +0530

    angular-task

--- angular-practice-project/src/app/services/todoserv.service.ts
+++ angular-practice-project/src/app/services/todoserv.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 import { todo } from '../todo.model';
 import { environment } from '../../environments/environment';
 
@@ -8,14 +9,28 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class TodoservService {
-  private readonly baseUrl = environment.baseurl;
-
+  private readonly baseurl = environment.baseurl;
 
   constructor(private http: HttpClient) {
 
    }
 
    addTodo(name:string, job:string):Observable<todo>{
-    return this.http.get()
+    return this.http.post<todo>(this.baseurl, { name, job });
    }
+
+   getTodos(): Observable<todo[]> {
+    return this.http.get<{ data: todo [] }>(this.baseurl).pipe(
+    map ((res:{data: todo []}) => res.data)
+    );
+  }
+
+    deleteTodo(id: string): Observable<todo> {
+      return this.http.delete<todo>(`${this.baseurl}/${id}`);
+    }
+
+    updateTodo(id: string, changes:todo): Observable<todo> {
+      return this.http.put<todo>(`${this.baseurl}/${id}`,changes );
+    }
+  
 }
--- angular-practice-project/src/app/todo.model.ts
+++ angular-practice-project/src/app/todo.model.ts
@@ -1,6 +1,6 @@
 export interface todo{
-_id: string;
-title: string;
-description: string;
-status: string;
+name: string;
+job: string;
+id: string;
+createdAt: string;
 }
