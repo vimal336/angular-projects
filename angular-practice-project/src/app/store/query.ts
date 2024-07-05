aef2fa271e6adcef1cb385eae4090235ef0bb99d
vimal336 <asvimal336@gmail.com>
2024-07-05 12:32:51 +0530

    angular-task

--- angular-practice-project/src/app/store/query.ts
+++ angular-practice-project/src/app/store/query.ts
@@ -1,10 +1,28 @@
 import { Query } from "@datorama/akita";
 import { TodoState, TodoStore  } from "./store";
+import { Observable } from "rxjs";
+import { todo } from "../todo.model";
 
 
 export class TodoQuery extends Query <TodoState> {
     constructor(private todoStore: TodoStore){
         super(todoStore);
     }
+
+getTodos(): Observable <todo[]>{
+    return this.select(state => state.todos);
 }
+
+
+getLoaded(): Observable<boolean> {
+    return this.select( state => state.isLoaded);
+}
+
+
+getLoading(): Observable<boolean> {
+    return this.selectLoading();
+    }
+}
+
+
 
