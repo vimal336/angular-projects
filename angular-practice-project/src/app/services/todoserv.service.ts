f583619e4cab1d17f70c5c4596cf6b5717637584
vimal336 <asvimal336@gmail.com>
2024-07-05 10:43:16 +0530

    angular-task

--- angular-practice-project/src/app/services/todoserv.service.ts
+++ angular-practice-project/src/app/services/todoserv.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { todo } from '../todo.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoservService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+   }
+
+   addTodo(title:string, description:string):Observable<todo>{
+    return this.http.get()
+   }
 }
