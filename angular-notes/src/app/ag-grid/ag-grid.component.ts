fc8b130ec74d57380ba085fe90b12cc6e4ea4880
vimal336 <asvimal336@gmail.com>
2024-07-09 15:41:42 +0530

    angular-rxjs

--- angular-notes/src/app/ag-grid/ag-grid.component.ts
+++ angular-notes/src/app/ag-grid/ag-grid.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-ag-grid',
   standalone: true,
-  imports: [],
+  imports: [AgGridModule,AgGridAngular],
   templateUrl: './ag-grid.component.html',
   styleUrl: './ag-grid.component.css'
 })
 export class AgGridComponent {
 
 }
+
