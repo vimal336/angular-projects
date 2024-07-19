eae9121dc55dacda757d098169b091c8e2263b58
vimal336 <asvimal336@gmail.com>
2024-07-19 12:10:57 +0530

    angular-rxjs

--- angular-notes/src/app/rxjs-debounce/rxjs-debounce.component.ts
+++ angular-notes/src/app/rxjs-debounce/rxjs-debounce.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { combineLatest, debounceTime } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
+import { MathsService } from '../services/maths.service';
 
 @Component({
   selector: 'app-rxjs-debounce',
@@ -10,20 +11,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './rxjs-debounce.component.html',
   styleUrl: './rxjs-debounce.component.css'
 })
-export class RxjsDebounceComponent implements OnInit {
+export class RxjsDebounceComponent {
   searchControl: FormControl;
   searchText: string = '';
-  searchControl2: FormControl<string | null>;
-  searchControl1: any;
-  combinedValue: string | undefined;
-
-  constructor() {
+  data: any;
+  constructor(private dataService: MathsService) {
     this.searchControl = new FormControl('');
-    this.searchControl = new FormControl('');
-    this.searchControl2 = new FormControl('');
   }
 
   ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      (results) => {
+        this.data = results;
+      },
+      (error) => {
+        console.error('Error fetching data', error);
+      }
+    );
     this.searchControl.valueChanges
       .pipe(
         debounceTime(1000) 
@@ -31,14 +35,6 @@ export class RxjsDebounceComponent implements OnInit {
       .subscribe(value => {
         this.searchText = value;
         console.log(value);
-      });
-
-      combineLatest([
-        this.searchControl1.valueChanges.pipe(debounceTime(300)),
-        this.searchControl2.valueChanges.pipe(debounceTime(300))
-      ]).subscribe(([value1, value2]) => {
-        this.combinedValue = `Value 1: ${value1}, Value 2: ${value2}`;
-        console.log(this.combinedValue); // You can replace this with your logic
       });
   }
 }
--- angular-notes/src/app/services/maths.service.ts
+++ angular-notes/src/app/services/maths.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,12 +8,20 @@ import { Injectable } from '@angular/core';
 export class MathsService {
 
   _msg="Service in angular";
-  constructor() {}
+  constructor(private http: HttpClient){}
 
   
 
   getmessage(){
     return this._msg;
+  }
+
+  getData() {
+    const request1 = this.http.get('https:jsonplaceholder.typicode.com/posts');
+    const request2 = this.http.get('https:jsonplaceholder.typicode.com/posts');
+    const request3 = this.http.get('https:jsonplaceholder.typicode.com/posts');
+
+    return forkJoin([request1, request2, request3]);
   }
 
   
