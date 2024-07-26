e4be82dedb7f08f296457838a82c143f90391b57
vimal336 <asvimal336@gmail.com>
2024-07-26 10:47:07 +0530

    implementing chart in angular

---
+++ angular-notes/src/app/angular-chart/angular-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, viewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-angular-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './angular-chart.component.html',
+  styleUrl: './angular-chart.component.css'
+})
+export class AngularChartComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+
+}
